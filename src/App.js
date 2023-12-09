5c4d0b1e702499126ca748b3bfb1b214427acfb3
Abhisheksb1003 <abhishekbikkannavar1@gmail.com>
2023-12-09 19:13:15 +0530

    Adding Contact US Page via REACT ROUTER

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
+// App.js
 import './App.css';
 import Product from './Product/Product';
 import Cart from './Cart/Cart';
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import Header from './UI/Header';
 import Itemprovider from './Store/Itemprovider';
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // Import Routes
+
 import About from './About/About';
 import Footer from './UI/Footer';
 import Home from './Home/Home';
-
-
-const router = createBrowserRouter([
-  {path :'/', element: <Product/>},
-  {path :'/store', element: <Product/>},
-  {path :'/about', element: <About/>},
-  {path :'/home', element: <Home/>},
-])
+import Contact from './Contact/Contact';
 
 function App(props) {
+  const [Hidecart, setHidecart] = useState(false);
 
-  const [Hidecart, setHidecart]= useState(false)
+  const showcart = () => {
+    setHidecart(true);
+  };
 
-  const showcart=()=>{
-    setHidecart(true)
-  }
-
-  const hidecart=()=>{
-    setHidecart(false)
-  }
+  const hidecart = () => {
+    setHidecart(false);
+  };
 
   return (
     <Itemprovider>
-      {Hidecart && <Cart onClose={hidecart}/>}
+      {Hidecart && <Cart onClose={hidecart} />}
       <Header onOpen={showcart} />
-      <RouterProvider router={router}/>
-      <Footer/>
 
+      {/* Wrap Routes around Route components */}
+      <Routes>
+        <Route path='/store' element={<Product />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+
+      <Footer />
     </Itemprovider>
   );
 }
--- src/UI/Header.js
+++ src/UI/Header.js
@@ -2,23 +2,31 @@ import React, { Fragment, useContext } from "react";
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import Itemstore from "../Store/Itemstore";
 import classes from './Header.module.css'
+import { NavLink } from "react-router-dom";
 
 const Header = (props) => {
   const cartctx = useContext(Itemstore);
-
   let totalcount = 0;
   cartctx.cart.forEach((cartitem) => {
     totalcount = totalcount + cartitem.quantity;
   });
-
   return (
     <Fragment>
       <Navbar bg="dark" expand="sm" variant="dark">
         <Container>
           <Nav>
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/Store">Store</Nav.Link>
-            <Nav.Link href="/About">About</Nav.Link>
+            <ul>
+              <NavLink to="/home">Home</NavLink>
+            </ul>
+            <ul>
+              <NavLink to="/store">Store</NavLink>
+            </ul>
+            <ul>
+              <NavLink to="/about">About</NavLink>
+            </ul>
+            <ul>
+              <NavLink to="/contact">Countact Us</NavLink>
+            </ul>
           </Nav>
           <Nav>
             <Navbar.Text>{totalcount}</Navbar.Text>
