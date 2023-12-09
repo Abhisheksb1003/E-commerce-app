a15ea28fe9bfdf665a4a733059dbd150bf8587e4
Abhisheksb1003 <abhishekbikkannavar1@gmail.com>
2023-12-09 19:44:59 +0530

    Implement the Products Page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Cart from './Cart/Cart';
 import React, { useState } from "react";
 import Header from './UI/Header';
 import Itemprovider from './Store/Itemprovider';
-import { Routes, Route } from 'react-router-dom'; // Import Routes
-
+import { Routes, Route, Navigate } from 'react-router-dom';
 import About from './About/About';
 import Footer from './UI/Footer';
 import Home from './Home/Home';
 import Contact from './Contact/Contact';
+import ProductDetails from './Product/ProductDetails';
 
 function App(props) {
   const [Hidecart, setHidecart] = useState(false);
@@ -28,12 +28,13 @@ function App(props) {
       {Hidecart && <Cart onClose={hidecart} />}
       <Header onOpen={showcart} />
 
-      {/* Wrap Routes around Route components */}
       <Routes>
+        <Route path='/' element={<Navigate to='/store' />} />
         <Route path='/store' element={<Product />} />
         <Route path='/about' element={<About />} />
         <Route path='/home' element={<Home />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/store/:productID' element={<ProductDetails />} />
       </Routes>
 
       <Footer />
--- src/Store/Itemprovider.js
+++ src/Store/Itemprovider.js
@@ -4,13 +4,69 @@ import Itemstore from "./Itemstore";
 
 const Itemprovider = (props) => {
   const [cartElements, setCartElements] = useState([]);
- 
+  //   const cartElements = [
+  //     {
+  //       title: "Colors",
+  //       price: 100,
+  //       imageUrl:
+  //         "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+  //       quantity: 2,
+  //     },
+  //     {
+  //       title: "Black and white Colors",
+  //       price: 50,
+  //       imageUrl:
+  //         "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+  //       quantity: 3,
+  //     },
+  //     {
+  //       title: "Yellow and Black Colors",
+  //       price: 70,
+  //       imageUrl:
+  //         "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+  //       quantity: 1,
+  //     },
+  //   ];
+
+
+
+
+  const productsArr = [
+    {
+      id: "m1",
+      title: "Colors",
+      price: 100,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+    },
+    {
+      id: "m2",
+      title: "Black and white Colors",
+      price: 50,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    },
+    {
+      id: "m3",
+      title: "Yellow and Black Colors",
+      price: 70,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+    },
+    {
+      id: "m4",
+      title: "Blue Color",
+      price: 100,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+    },
+  ];
+
 
   const addtocartHandler = (item) => {
     const existingCartItem = cartElements.find(
       (cartitem) => cartitem.id === item.id
     );
-
     if (existingCartItem) {
       alert('item alredy exists')
     } else {
@@ -20,6 +76,7 @@ const Itemprovider = (props) => {
 
   const itemstore = {
     cart: cartElements,
+    product:productsArr,
     addToCart: addtocartHandler,
   };
 
